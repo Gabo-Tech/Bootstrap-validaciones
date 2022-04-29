7bd5807f730e0ca17a4d01716b4671aa7293a9c0
franpd8 <102958579+franpd8@users.noreply.github.com>
2022-04-29 13:21:12 +0200

    required eliminados y variables de usuario de js creadas

--- main.js
+++ main.js
@@ -1,77 +1,24 @@
-body {
-	color: #999;
-	background: #e2e2e2;
-	font-family: 'Roboto', sans-serif;
+// alert("ey")
+
+// recoger datos del usuario
+const getName= document.querySelector('#name')
+const getEmail=document.querySelector('#email')
+const getPass1=document.querySelector('#password')
+const getPass2=document.querySelector('#password2')
+
+const btn=document.querySelector('#submit')
+console.log(btn)
+
+// evento para recoger informacion
+
+btn.addEventListener("submit", getUser);
+
+// funcion para coger datos
+function getUser(e){
+    e.preventDefault()
+    console.log(`Mi nombre es ${getName.value} y mi correo es ${getEmail.value}`)
+
 }
-.form-control {
-	min-height: 41px;
-	box-shadow: none;
-	border-color: #e1e1e1;
-}
-.form-control:focus {
-	border-color: #cb0000;
-}	
-.form-control, .btn {        
-	border-radius: 3px;
-}
-.form-header {
-	margin: -30px -30px 20px;
-	padding: 30px 30px 10px;
-	text-align: center;
-	background: #cb0000;
-	border-bottom: 1px solid #eee;
-	color: #fff;
-}
-.form-header h2 {
-	font-size: 34px;
-	font-weight: bold;
-	margin: 0 0 10px;
-	font-family: 'Pacifico', sans-serif;
-}
-.form-header p {
-	margin: 20px 0 15px;
-	font-size: 17px;
-	line-height: normal;
-	font-family: 'Courgette', sans-serif;
-}
-.signup-form {
-	width: 390px;
-	margin: 0 auto;	
-	padding: 30px 0;	
-}
-.signup-form form {
-	color: #999;
-	border-radius: 3px;
-	margin-bottom: 15px;
-	background: #f0f0f0;
-	box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.3);
-	padding: 30px;
-}
-.signup-form .form-group {
-	margin-bottom: 20px;
-}		
-.signup-form label {
-	font-weight: normal;
-	font-size: 14px;
-}
-.signup-form input[type="checkbox"] {
-	position: relative;
-	top: 1px;
-}
-.signup-form .btn {        
-	font-size: 16px;
-	font-weight: bold;
-	background: #cb0000;
-	border: none;
-	min-width: 200px;
-}
-.signup-form .btn:hover, .signup-form .btn:focus {
-	background: #b00000 !important;
-	outline: none;
-}
-.signup-form a {
-	color: #cb0000;		
-}
-.signup-form a:hover {
-	text-decoration: underline;
-}
+
+
+
