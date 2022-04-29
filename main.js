2faf826fc37d5e7a653633e2d4089833fdd1b7eb
franpd8 <102958579+franpd8@users.noreply.github.com>
2022-04-29 13:55:46 +0200

    funcion recoger usuarios lista

--- main.js
+++ main.js
@@ -5,20 +5,33 @@ const getName= document.querySelector('#name')
 const getEmail=document.querySelector('#email')
 const getPass1=document.querySelector('#password')
 const getPass2=document.querySelector('#password2')
+const btn=document.querySelector('#form')
+// console.log(btn)
 
-const btn=document.querySelector('#submit')
-console.log(btn)
+
+let users =[]
 
 // evento para recoger informacion
 
-btn.addEventListener("submit", getUser);
+btn.addEventListener('submit', getUser);
 
 // funcion para coger datos
 function getUser(e){
     e.preventDefault()
-    console.log(`Mi nombre es ${getName.value} y mi correo es ${getEmail.value}`)
+
+    let userFields = {
+        name: getName.value,
+        email: getEmail.value,
+        password1: getPass1.value,
+        password2: getPass2.value
+      };
+
+    console.log(`Nuevo usuario — Nombre: ${userFields.name}, correo:${userFields.email}`)
+    users.push(userFields);
+    console.log("Usuarios recogidos",users)
 
 }
+
 
 
 
