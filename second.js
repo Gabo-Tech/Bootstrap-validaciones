9976d62a60433729562c2ac4504bdc1c41c9ff5f
franpd8 <102958579+franpd8@users.noreply.github.com>
2022-05-02 10:37:35 +0200

    cambios en el localStorage

--- second.js
+++ second.js
@@ -1,31 +1,32 @@
 // alert("ey")
 const listadoUsuarios=document.querySelector('#all-users')
 console.log(listadoUsuarios) 
-let infoUser = JSON.parse(localStorage.getItem("users"));
+let infoUser = JSON.parse(localStorage.getItem("users")) ;
 console.log(infoUser)
 
 function printUsers(){
-let infoUser = JSON.parse(localStorage.getItem("users"));
+    listadoUsuarios.innerHTML += ``
+    let infoUser = JSON.parse(localStorage.getItem("users"));
 
-    for(let i = 0; i < infoUser.length; i++){
-  
-      listadoUsuarios.innerHTML += `
-                              <div class="col-sm-6">
-                                  <div class="card">
-                                    <div class="card-body">
-                                      <h5 class="card-title">
-                                        ${infoUser[i].name}</h5>
-                                      <p class="card-text">
-                                        ${infoUser[i].email}
-                                      </p>
+        for(let i = 0; i < infoUser.length; i++){
+      
+          listadoUsuarios.innerHTML += `
+                                  <div class="col-sm-6">
+                                      <div class="card">
+                                        <div class="card-body">
+                                          <h5 class="card-title">
+                                            ${infoUser[i].name}</h5>
+                                          <p class="card-text">
+                                            ${infoUser[i].email}
+                                          </p>
+                                        </div>
+                                      </div>
                                     </div>
-                                  </div>
-                                </div>
-                                                `
-    }
-    
-  }
+                                                    `
+        }
+        
+      }
 
-  window.onload = function() {
-    printUsers ()
+    window.onload = function() {
+        printUsers ()
   };
